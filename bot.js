70a4014aabb55c46e9e3ce7de44aad52cebef4aa
DayofSolace <33674746+DayofSolace@users.noreply.github.com>
2018-05-01 10:05:38 -0700

    added command manager setup

--- bot.js
+++ bot.js
@@ -1,13 +1,35 @@
 const Discord = require('discord.js'),
     config = require('./config.json'),
+    fs = require('fs'),
     token = config.token,
     prefix = config.prefix,
     bot = new Discord.Client({
         disableEveryone: true
     });
 
+bot.commands = new Discord.Collection();
+
+fs.readdir('./cmds/', (err, files) => {
+    if (err) console.error(err);
+
+    let jsfiles = files.filter(f => f.split('.').pop() === 'js');
+    if (jsfiles.length <= 0) {
+        console.log('No commands to load!');
+        return;
+    }
+
+    console.log(`Loading ${jsfiles.length} commands`);
+
+    jsfiles.forEach((f, i) => {
+        let props = require(`./cmds/${f}`);
+        console.log(`${i + 1}: ${f} loaded!`);
+        bot.commands.set(props.help.name, props);
+    });
+});
+
 bot.on('ready', async () => {
     console.log(`==== ${bot.user.username} Bot is online ====`);
+    console.log(bot.commands);
 });
 
 bot.on('message', async message => {
@@ -17,23 +39,11 @@ bot.on('message', async message => {
 
     let messageArray = message.content.substring(prefix.length).split(" "),
         command = messageArray[0],
-        args = messageArray.slice(1);
+        args = messageArray.slice(1),
+        cmd = bot.commands.get(command);
 
-    switch(command.toLowerCase()){
-        case 'myinfo':
-            let embed = new Discord.RichEmbed()
-                .setAuthor(message.author.username)
-                .setDescription("This is the user's info!")
-                .setColor("#9B59B6")
-                .addField("Full Username", `${message.author.username}#${message.author.discriminator}`)
-                .addField("ID", message.author.id)
-                .addField("Created At", message.author.createdAt);
-            
-            message.channel.send(embed);
-            break;
-        default:
-            message.channel.send("Invalid Command.");
-    }
+    if(cmd) cmd.run(bot, message, args);
+    else message.channel.send('Invalid Command!');
 });
 
 bot.login(token);
